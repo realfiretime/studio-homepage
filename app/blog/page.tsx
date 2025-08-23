53a4788e8e57da3859e12d1ff44b6c2be83af45f
FireTime <firetime@firetime.me>
2025-08-23 19:08:03 -0400

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
   title: 'Blog',
-  description: 'Read my blog.',
+  description: 'games and stuff. i guess',
 }
 
 export default function Page() {
