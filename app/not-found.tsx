141bef6ab06fdd7ae7bb3eb08c0e3802feba6fe6
FireTime <firetime@firetime.me>
2025-08-23 19:15:20 -0400

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,12 @@ export default function NotFound() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="mb-4">Hey, so how did you get here? your annoyed. i can tell. ok lisen. you were prbly sent here. i dont know why. but i think.
+                          there are probly around 3 reasons why your here. 1, the page your looking for cant be found/ does not exist
+                          2, you were sent an old link to a blog of which the name was changed or something. or 3, your firend sent you this
+                          page and told your something like "its a new game" while we are working on a 404 - not found game ..... wait 
+                          if we try and make a page for that, its going to redirect here..... doesent matter, anyways just go back
+                          to the root URL which is https://studio.firetime.me *you dont need to include the https if you dont want* </p>
     </section>
   )
 }
